1f2c74cbaa6a1f4709eb08d0388b069bbe9dc236
unluckid <144270549+unluckid@users.noreply.github.com>
2024-03-13 13:47:03 +0900

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -4,8 +4,11 @@
 // @TODO: 10개의 숫자로 배열 만들기
 let numbers= [1,2,3,4,5,6,7,8,9,10];
 // @TODO: printNumbers 함수를 만들어 주세요.
-let printNumbers;
-
+funtion printNumbers(numbers){
+  for (let count = 0; count<numbers.length; count++){
+  console.log(numbers[count])}
+}
+printNumbers(numbers);
 
 
 
