b5bd75eb118c86656b98f833268f013ad8dd80d6
unluckid <144270549+unluckid@users.noreply.github.com>
2024-03-13 13:54:29 +0900

    Update messages.js

--- messages.js
+++ messages.js
@@ -2,12 +2,9 @@
 "use strict";
 
 // @TODO: 3개의 문자열 messages 배열 만들어 주세요.
-let messages=[];
-messages[0] ="안녕";
-messages[1] ="하루";
-messages[2] ="커피";
+let messages = ["안녕", "하루", "커피"];
 // @TODO: messages 배열에서 각 메시지를 인쇄하는 함수를 만들어 주세요.
-funtion printMsgs(messages){
+let printMsgs= function printMsgs(messages){
   for (let count = 0; count<messages.length; count++){
   console.log(messages[count])}
 }
